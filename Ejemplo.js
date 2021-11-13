ce8246cffcb9d5667a554dd166eecc0a27957b9c
hectorbalan <balan.huchin.3@gmail.com>
2021-11-13 08:44:55 -0600

    changes

--- Ejemplo.js
+++ Ejemplo.js
@@ -1,1 +1,12 @@
 console.log("Este es un ejemplo para git hub");
+//soy un comentario
+
+/**
+ echo "# EjemploRepo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/hector4like6gorillaz/EjemploRepo.git
+git push -u origin main
+ */
